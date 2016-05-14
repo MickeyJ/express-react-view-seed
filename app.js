9603a89dd4a426a61b745749c94603f1d01c7fb6
Mickey <develop.agility@gmail.com>
2016-05-14 16:44:12 -0600

    trying various things

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const reactExpress = require('express-react-views');
 
 const routes = require('./routes/index');
 
@@ -14,7 +13,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('jsx',
-  reactExpress
+  require('express-react-views')
     .createEngine({ beautify: true})
 );
 app.use(logger('dev'));
@@ -30,15 +29,15 @@ app.use( (req, res, next) =>{
   next(err);
 });
 
-if(app.get('env') === 'development') {
-  app.use((err, req, res, next) =>{
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if(app.get('env') === 'development') {
+//   app.use((err, req, res, next) =>{
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 app.use( (err, req, res, next) =>{
   res.status(err.status || 500);
