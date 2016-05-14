c15f3c020f7087a9c253d6aa3bb10317088e2867
Mickey <develop.agility@gmail.com>
2016-05-14 16:26:39 -0600

    created html head component

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 
+const Head = require('./common/Head');
 const NavHeader = require('./common/NavHeader');
 const Footer = require('./common/Footer');
 
@@ -9,15 +10,11 @@ const Layout = React.createClass({
   render: function(){
     return (
       <html lang="en">
-      <head>
-        <title>{this.props.title}</title>
-        <meta name="description" content={this.props.description}/>
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-        <link rel="stylesheet" href="bower_components/bootstrap-css/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="css/main.css"/>
-      </head>
+      <Head
+        siteName={siteName}
+        title={this.props.title}
+        description={this.props.description}
+      />
       <body>
         <div id="all-wrap">
 
